af4e33bab7e83b72044908526fa71dd137f35aad
iqra-dev444 <iali952002@gmail.com>
2026-09-22 18:36:15 +0530

    Add accessible dialog functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const dialog = document.getElementById("info-dialog");
+const closeButton = dialog?.querySelector("button");
 
+if (dialog && closeButton) {
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+    });
+}
